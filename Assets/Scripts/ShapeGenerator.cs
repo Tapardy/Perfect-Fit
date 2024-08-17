3b8851d477677e646322321324e07b7bbe626e82
Víctor <sentym0@gmail.com>
2024-08-17 12:22:40 +0100

    Fixed shape generation

--- Assets/Scripts/ShapeGenerator.cs
+++ Assets/Scripts/ShapeGenerator.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class ShapeGenerator : MonoBehaviour
 {
+    public GameObject shape;
     public GameObject cube;
-    public int width = 10;
-    public int height = 4;
+    public int width;
+    public int height;
+    public int startX;
+    public int startY;
+
     void Start()
     {
+        Instantiate(shape, new Vector3(Random.Range(0, width-1 + startX), Random.Range(0, height-1 + startY), gameObject.transform.position.z), Quaternion.identity, gameObject.transform);
         for (int y = 0; y < height; ++y)
         {
             for (int x = 0; x < width; ++x)
             {
-                Instantiate(cube, new Vector3(x, y, 0), Quaternion.identity);
+                Instantiate(cube, new Vector3(x + startX, y + startY, gameObject.transform.position.z), Quaternion.identity, gameObject.transform);
             }
         }
     }
