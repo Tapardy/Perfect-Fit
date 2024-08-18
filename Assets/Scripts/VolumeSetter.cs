a098560b1125aaaeb7e02a5da4896cf664d017b7
Víctor <sentym0@gmail.com>
2024-08-18 14:59:34 +0100

    Main Menu code and placeholder elements

--- Assets/Scripts/VolumeSetter.cs
+++ Assets/Scripts/VolumeSetter.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class VolumeSetter : MonoBehaviour
 {
-    public void SetVolume(float volume)
+    public AudioMixer audioMixer;
+    public void SetMasterVolume(float volume)
     {
-
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
     }
 }
