a559943a9cda8de3b052e79e1151e3206990fea6
Víctor <sentym0@gmail.com>
2024-08-18 12:18:26 +0100

    Settings menu placeholder UI

---
+++ Assets/Scripts/VolumeSetter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeSetter : MonoBehaviour
+{
+    public void SetVolume(float volume)
+    {
+
+    }
+}
