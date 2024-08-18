043698725ac5626968d4b6f1932ad7d7b8d89745
Tapardy <117444657+Tapardy@users.noreply.github.com>
2024-08-18 15:11:11 +0200

    first version of score system via shape detection
    Shape detection works, but the player can manipulate this in ways I dont like, then again, uh manipulating requires effort and might as well not matter. Will still try and fix that tho

--- Assets/Scripts/CubeDestroyer.cs
+++ Assets/Scripts/CubeDestroyer.cs
@@ -6,7 +6,10 @@ public class CubeDestroyer : MonoBehaviour
 {
     public int cubesDestroyed;
     public int childCount;
-    
+    public int score;
+    private int playerCollisionCount;
+    private bool isPerfectFit = false;
+
     public void ChildChecker()
     {
         childCount = gameObject.transform.childCount;
@@ -20,7 +23,63 @@ public class CubeDestroyer : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        cubesDestroyed++;
-        Destroy(other.gameObject);
+        if (other.CompareTag("Player"))
+        {
+            playerCollisionCount++;
+            if (playerCollisionCount == childCount)
+            {
+                isPerfectFit = true;
+            }
+        }
+        else
+        {
+            cubesDestroyed++;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (isPerfectFit)
+            {
+                score += 1000;
+                Debug.Log("Perfect Fit! Added 1000 points. Total score: " + score);
+                isPerfectFit = false;
+            }
+            else
+            {
+                AddScore();
+            }
+            playerCollisionCount = 0; // Reset the count for the next interaction
+        }
+    }
+
+    private void AddScore()
+    {
+        int pointsToAdd = 0;
+
+        switch (playerCollisionCount)
+        {
+            case 1:
+                pointsToAdd = 100;
+                break;
+            case 2:
+                pointsToAdd = 300;
+                break;
+            case 3:
+                pointsToAdd = 500;
+                break;
+            case 4:
+                pointsToAdd = 700;
+                break;
+            case 5:
+                pointsToAdd = 1000;
+                break;
+        }
+
+        score += pointsToAdd;
+        Debug.Log("Added " + pointsToAdd + " points. Total score: " + score);
     }
 }
